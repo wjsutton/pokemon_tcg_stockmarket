be8f6dec0f2274ebd0f9f609322176cd9afdce1a
Will Sutton <wjsutton.dev@outlook.com>
2025-03-01 08:05:23 +0000

    march data

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ modern_queries = ["set.id:swsh*", "set.id:sv*"]
 def fetch_and_save_cards(queries, filename):
     dfs = [fetch_pokemon_cards(api_key, query) for query in queries]
     combined_df = pd.concat(dfs, ignore_index=True)
-    combined_df.to_csv(f"data/{filename}", index=False)
+    combined_df.to_csv(f"data/{filename}", encoding='utf-8', index=False)
     print(f"Saved {filename} with {len(combined_df)} cards.")
 
 # Fetch and save vintage and modern cards
-fetch_and_save_cards(vintage_queries, "vintage_pkmn_cards_feb2025.csv")
-fetch_and_save_cards(modern_queries, "modern_pkmn_cards_feb2025.csv")
+fetch_and_save_cards(vintage_queries, "vintage_pkmn_cards_mar2025.csv")
+fetch_and_save_cards(modern_queries, "modern_pkmn_cards_mar2025.csv")
