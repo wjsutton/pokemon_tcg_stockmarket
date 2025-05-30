7c6dd32a496ac35c789c74b7fa750d7f7db37aad
Will Sutton <wjsutton.dev@outlook.com>
2025-05-30 10:01:36 +0100

    adding kestra support

--- kestra.py
+++ kestra.py
@@ -11,12 +11,6 @@ api_key = os.getenv("TCG_API_KEY")
 if not api_key:
     print("TCG_API_KEY is missing! Make sure to set it in your .env file.")
 
-# Finds early sets (Base and Gym Heroes)
-vintage_queries = ["set.id:base*", "set.id:gym*"]
-
-# Find all cards from Sword & Shield + Scarlet & Violet eras
-modern_queries = ["set.id:swsh*", "set.id:sv*"]
-
 # Function to fetch cards data and save it in the folder 'data/'
 def fetch_and_save_cards(queries, base_filename):
     dfs = [fetch_pokemon_cards(api_key, query) for query in queries]
@@ -29,6 +23,7 @@ def fetch_and_save_cards(queries, base_filename):
     os.makedirs("data", exist_ok=True)
     combined_df.to_csv(f"data/{filename}", encoding='utf-8', index=False)
 
-# Fetch and save vintage and modern cards
-fetch_and_save_cards(vintage_queries, "vintage_pkmn_cards.csv")
-fetch_and_save_cards(modern_queries, "modern_pkmn_cards.csv")
+# Finds Base Set
+base_set = ["set.id:base*"]
+
+fetch_and_save_cards(base_set, "base_set_pkmn_cards.csv")
